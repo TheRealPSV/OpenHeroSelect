7d2719ad6ad89e045d8a05f4919eec58ff4a6585
ak2yny <92672223+ak2yny@users.noreply.github.com>
2022-03-02 08:42:49 +0100

    added option for output argument in xml2json

--- python_source/xml to json converter (BaconWizard17).py
+++ python_source/xml to json converter (BaconWizard17).py
@@ -10,7 +10,6 @@ def convert_escape(old_line):
     return new_line
 
 def convert(file_name: Path):
-    file_name = Path(file_name)
     # start by opening the file
     with open(file_name, mode='r') as file:
         lines_output = []
@@ -49,27 +48,30 @@ def convert(file_name: Path):
         lines_output[-1] = lines_output[-1].strip(',')
         lines_output.append('}')
 
-    # this determines the output file name
-    file_name_out = file_name.with_suffix('.json')
-
     # once the conversion is done, you can write it all to the new file        
     with open(file_name_out, mode = 'w') as file:
         for line in lines_output:
             file.write(line)
             file.write('\n')
 
-# read one argument as input
+# read optional argument as input, another optional as output
 parser = ArgumentParser()
 parser.add_argument('input', nargs='?', default='input?', help='input file (supports glob)')
+parser.add_argument('output', nargs='?', default='*.json', help='output file (wildcards will be replaced by input file name)')
 args = parser.parse_args()
 input_files = glob.glob(args.input, recursive=True)
 
-# if no argument given or found, ask for input, else process all
+# if no argument given or found, ask for input, else process argument
 if not input_files:
     input_files = 'input?'
 if input_files == 'input?':
     file_name = input('File to Convert with extension (ex: herostat.txt)> ')
+    file_name = Path(file_name)
+    file_name_out = file_name.with_suffix('.json')
     convert(file_name)
 else:
     for file_name in input_files:
+        file_name_out = args.output
+        if '*' in file_name_out:
+            file_name_out = file_name_out.replace('*', Path(file_name).stem)
         convert(file_name)
