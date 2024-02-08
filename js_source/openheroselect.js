2c5342d4d3a9483276181390a98e512b1a2f11e2
ak2yny <92672223+ak2yny@users.noreply.github.com>
2024-02-09 00:03:25 +0100

    Fixed script unlocks
    - Fixed a bug where unlock scripts are added at the end of new_game.py, which causes characters to be unlocked too late (after getting to the initial stage) and [blocks automatic progression from the intro video](https://discord.com/channels/449510825385000960/449515176312569856/1205218807912865833).
    - Changed default 50RH characters to match the current 50RH roster (recently changed in the 50RH).

--- js_source/openheroselect.js
+++ js_source/openheroselect.js
@@ -843,16 +843,23 @@ function compileRavenFormats(data, filename, ext) {
 
 function writeUnlockScripts(filename, unlocks) {
   const newScriptlines = [];
+  const lastScriptlines = [];
   const unlockScriptFile = path.resolve("temp", "new_game.py");
   const scriptFile = fs.readFileSync(filename, "utf8");
   const scriptlines = scriptFile.split(/\r?\n/m);
   //copy other script lines that are not unlocks
+  let last = false;
   for (const scriptline of scriptlines) {
-    if (!scriptline.includes("unlockCharacter(")) {
-      newScriptlines.push(scriptline);
+    if (!scriptline.match(/unlockCharacter\(/i)) {
+      if (scriptline.match(/^\s*(loadZoneAddTeam|loadMapStartGame)/i) || last) {
+        lastScriptlines.push(scriptline);
+        last = true;
+      } else {
+        newScriptlines.push(scriptline);
+      }
     }
   }
-  Array.prototype.push.apply(newScriptlines, unlocks);
+  Array.prototype.push.apply(newScriptlines, unlocks.concat(lastScriptlines));
   fs.writeFileSync(unlockScriptFile, newScriptlines.join("\r\n"));
   fs.copyFileSync(unlockScriptFile, filename);
 }
